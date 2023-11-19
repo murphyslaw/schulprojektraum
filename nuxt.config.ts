96999d5f0edea96cabb2359450da62c191d92d67
Falk Köppe <falk.koeppe@gmail.com>
2023-11-19 22:05:19 +0100

    add icons and basic meta information

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "de",
+      },
+      meta: [
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          name: "description",
+          content:
+            "Sicherer, digitaler Raum für Schulen zur Präsentation der Projekte ihrer Schüler*innen.",
+        },
+      ],
+      title: "Schulprojektraum",
+      link: [
+        {
+          rel: "icon",
+          href: "/favicon.ico",
+          type: "image/x-icon",
+          sizes: "32x32",
+        },
+        {
+          rel: "icon",
+          href: "icon.svg",
+          type: "image/svg+xml",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/apple-touch-icon",
+        },
+        {
+          rel: "manifest",
+          href: "/manifest.webmanifest",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   typescript: {
     strict: true,
